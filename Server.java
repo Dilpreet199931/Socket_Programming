6b8fb58dc32c85998bf74056b5e4a97a375dc53d
Dilpreet Kaur <76248565+Dilpreet199931@users.noreply.github.com>
2021-05-01 23:00:27 +0530

    Initial Commit

---
+++ Server.java
@@ -0,0 +1,39 @@
+/*
+author : Dilpreet kaur
+*/
+
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server {
+	public static final int port = 9090;
+	private static ArrayList<Two_way_multiple_Client_at_a_time_Server> clients = new ArrayList<>();
+	private static ExecutorService pool = Executors.newFixedThreadPool(20);
+
+	public Server() {
+	}
+
+	public static void main(String[] args) throws Exception {
+		ServerSocket listener;
+		Socket client;
+		PrintWriter out;
+		BufferedReader in;
+		String request = "";
+		listener = new ServerSocket(port);
+		System.out.println("Server is waiting for connect...");
+
+		while(true) {
+			client = listener.accept();
+			System.out.println("yes...connected");
+			Two_way_multiple_Client_at_a_time_Server clientThread = new Two_way_multiple_Client_at_a_time_Server(client);
+			clients.add(clientThread);
+
+			pool.execute(clientThread);
+		}
+	}
+}
